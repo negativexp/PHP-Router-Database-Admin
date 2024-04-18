41e4f54e8fd79af98050fe40a7c30a74a0978417
newoutsider <90830312+negativexp@users.noreply.github.com>
2024-04-18 19:10:49 +0200

    uz me to nebavi

--- actions/admin/database/addTable.php
+++ actions/admin/database/addTable.php
@@ -1,60 +1,38 @@
 <?php
-//echo "Table Name: " . $_POST['tableName'] . "<br>";
-//if(isset($_POST['name'])) {
-//    $columns = $_POST['name'];
-//    $types = $_POST['type'];
-//    $isNull = $_POST['isNull'];
-//    $autoIncrement = isset($_POST['autoIncrement']) ? $_POST['autoIncrement'] : array();
-//
-//    echo "<table border='1'>";
-//    echo "<tr><th>Column Name</th><th>Data Type</th><th>Not Null</th><th>Auto Increment</th></tr>";
-//
-//    for ($i = 0; $i < count($columns); $i++) {
-//        echo "<tr>";
-//        echo "<td>" . $columns[$i] . "</td>";
-//        echo "<td>" . $types[$i] . "</td>";
-//        echo "<td>" . ($isNull[$i] == 'true' ? "Yes" : "No") . "</td>";
-//        echo "<td>" . (in_array($i, array_keys($autoIncrement)) ? "Yes" : "No") . "</td>"; // Check if current index is in array keys
-//        echo "</tr>";
-//    }
-//    echo "</table>";
-//}
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Retrieve table name
-    $tableName = $_POST['tableName'];
+$tableName = $_POST['tableName'];
+$columns = $_POST['name'];
+$types = $_POST['type'];
+$isNull = $_POST['isNull'];
+$autoIncrement = isset($_POST['autoIncrement']) ? $_POST['autoIncrement'] : array();
 
-    // Retrieve column details
-    $columns = $_POST['name'];
-    $types = $_POST['type'];
-    $isNull = $_POST['isNull'];
-    $autoIncrement = isset($_POST['autoIncrement']) ? $_POST['autoIncrement'] : array();
+// Construct SQL query
+$sql = "CREATE TABLE $tableName (";
 
-    // Construct SQL query
-    $sql = "CREATE TABLE $tableName (";
+for ($i = 0; $i < count($columns); $i++) {
+    $sql .= $columns[$i] . " " . $types[$i];
 
-    for ($i = 0; $i < count($columns); $i++) {
-        $sql .= $columns[$i] . " " . $types[$i];
-
-        if (in_array($i, array_keys($autoIncrement))) {
-            $sql .= " AUTO_INCREMENT";
-        } else {
-            if ($isNull[$i] == 'true') {
-                $sql .= " NOT NULL";
-            } else {
-                $sql .= " NULL";
-            }
+    if (in_array($i, array_keys($autoIncrement))) {
+        $sql .= " AUTO_INCREMENT";
+        if($columns[$i] == "id") {
+            $sql .= " PRIMARY KEY";
         }
-
-        if ($i < count($columns) - 1) {
-            $sql .= ", ";
+    } else {
+        if ($isNull[$i] == 'true') {
+            $sql .= " NOT NULL";
+        } else {
+            $sql .= " NULL";
         }
     }
 
-    $sql .= ");";
-
-    // Execute the query (you'll need your MySQL connection here)
-    // mysqli_query($conn, $sql);
-
-    echo "Table created successfully with the following query: <br>";
-    echo $sql;
+    if ($i < count($columns) - 1) {
+        $sql .= ", ";
+    }
 }
+
+$sql .= ");";
+
+include_once("db.php");
+$db = new Database();
+$params = [];
+$db->executeQuery($sql, $params, false);
+header("location: /admin/database");
--- db.php
+++ db.php
@@ -23,42 +23,47 @@ class Database {
 
     // for non-select queries, set returnResult to false on call.
     public function executeQuery($sql, $params = [], $returnResult = true) {
-        $stmt = $this->conn->prepare($sql);
-        if ($stmt === false) {
-            die("Error in prepare statement: " . $this->conn->error);
-        }
-        if (!empty($params)) {
-            $types = '';
-            $bindParams = [&$types];
-            foreach ($params as &$param) {
-                if (is_int($param)) {
-                    $types .= 'i';
-                } elseif (is_float($param)) {
-                    $types .= 'd';
-                } elseif (is_string($param)) {
-                    $types .= 's';
-                } else {
-                    $types .= 'b';
+        try {
+            $stmt = $this->conn->prepare($sql);
+            if ($stmt === false) {
+                throw new Exception("SQL error: " . $this->conn->error);
+            }
+            if (!empty($params)) {
+                $types = '';
+                $bindParams = [&$types];
+                foreach ($params as &$param) {
+                    if (is_int($param)) {
+                        $types .= 'i';
+                    } elseif (is_float($param)) {
+                        $types .= 'd';
+                    } elseif (is_string($param)) {
+                        $types .= 's';
+                    } else {
+                        $types .= 'b';
+                    }
+                    $bindParams[] = &$param;
                 }
-                $bindParams[] = &$param;
+                call_user_func_array([$stmt, 'bind_param'], $bindParams);
             }
-            call_user_func_array([$stmt, 'bind_param'], $bindParams);
-        }
-        $stmt->execute();
-        if ($stmt->error) {
-            die("Error in execute statement: " . $stmt->error);
-        }
-        if ($returnResult) {
-            $result = $stmt->get_result();
-            if ($result === false) {
-                die("Error in getting result: " . $stmt->error);
+            $stmt->execute();
+            if ($stmt->error) {
+                die("Error in execute statement: " . $stmt->error);
             }
-            return $result;
-        }
+            if ($returnResult) {
+                $result = $stmt->get_result();
+                if ($result === false) {
+                    die("Error in getting result: " . $stmt->error);
+                }
+                return $result;
+            }
 
-        $success = $stmt->affected_rows > 0;
-        $stmt->close();
-        return $success;
+            $success = $stmt->affected_rows > 0;
+            $stmt->close();
+            return $success;
+        } catch (mysqli_sql_exception $e) {
+            echo "<p><b>mysqli_sql_exception</b>: {$e->getMessage()}</p>";
+            die();
+        }
     }
     public function fetchRows($result) {
         $rows = [];
