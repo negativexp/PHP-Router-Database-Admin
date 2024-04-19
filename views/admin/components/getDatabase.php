f4adcf9df445d8111b1672ab931b9d9e18d584ff
Matyáš Schuller <matyas.schuller@educhem.cz>
2024-04-19 12:16:44 +0200

    bruh

--- views/admin/components/getDatabase.php
+++ views/admin/components/getDatabase.php
@@ -1,4 +1,28 @@
 <?php
 if(isset($name)) {
-    echo $name;
+    echo "<h1>Tabulka: {$name}</h1>";
+    include_once("db.php");
+    $db = new Database();
+    $sql = "SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = '{$name}'";
+    $columns = $db->fetchRows($db->executeQuery($sql));
+    $sql = "select * from {$name}";
+    $rows = $db->fetchRows($db->executeQuery($sql));
+    echo "<table border='1'>";
+    echo "<thead>";
+    echo "<tr>";
+    foreach($columns as $column) {
+        echo "<th>{$column["COLUMN_NAME"]}</th>";
+    }
+    echo "</tr>";
+    echo "</thead>";
+    echo "<tbody>";
+    foreach ($rows as $row) {
+        echo "<tr>";
+        foreach ($row as $columnValue) {
+            echo "<td>{$columnValue}</td>";
+        }
+        echo "</tr>";
+    }
+    echo "</tbody>";
+    echo "</table>";
 }
