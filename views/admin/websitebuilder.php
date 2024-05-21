acdd67e35c9e54926ccab733ea2f0890bcf7f636
newoutsider <90830312+negativexp@users.noreply.github.com>
2024-05-21 22:11:18 +0200

    mrdam to

--- views/admin/websitebuilder.php
+++ views/admin/websitebuilder.php
@@ -31,217 +31,116 @@
         <h1 class="big">Website Builder (lolíky)</h1>
     </header>
     <style>
-        #code-editor-content {
-            width: 100%;
-            display: flex;
-            flex-flow: column;
+        #richtextbox-blocks .block {
+            transition: border .2s ease;
+            border: 1px dashed rgba(128, 128, 128, 0.3);
         }
-        #code-editor-content .wrapper-content {
-            padding: 0;
+        #richtextbox-blocks .block:hover {
+            border: 1px dashed rgba(128, 128, 128, 1);
         }
-        #code-editor-content .block {
-            border-radius: 7px;
-            border: 1px solid white;
-            display: flex;
-            align-items: center;
+        #richtextbox-blocks .block section:first-child{
+            padding-bottom: 20px;
         }
-        #code-editor-content .block:hover {
-            border: 1px solid rgb(128, 128, 128);
+        #richtextbox-blocks .block * {
+            all: revert;
         }
-        #code-editor-content .block:hover a {
-            display: block !important;
+        #richtextbox-blocks .active {
+            border: 1px solid red;
         }
-
-        #code-editor-content .block span {
-            padding: 10px;
-            text-align: right;
-            opacity: 0.3;
-            transition: opacity .2s ease;
-        }
-        #code-editor-content .block:hover span {
-            opacity: 1;
-        }
-        #code-editor-content .block *:nth-child(2) {
-            flex: 1;
-        }
-        #code-editor-content .block a {
-            display: none;
-        }
-        #code-editor-content .block textarea {
-            width: 100%;
-        }
-        .s1 {
-            border: 1px solid black;
-            height: 100px;
-        }
-        .s2 {
-            border: 1px solid black;
-            height: 100px;
-        }
-        .s3 {
-            border: 1px solid black;
-            height: 100px;
-        }
-        .s4 {
-            border: 1px solid black;
-            height: 100px;
-        }
-        .w100 {
-            width: 100%;
-            height: 100px;
-            background: red;
-        }
-        .w50 {
-            width: 50%;
-            background: yellow;
-        }
-        .w33 {
-            width: 33.333%;
-            background: orange;
-        }
-        .w25 {
-            width: 25%;
-            background: purple;
-        }
+        #richtextbox-blocks .block section div {height: 20px; border: 1px solid black;}
+        #richtextbox-blocks .block .w100 { width: 100%; }
+        #richtextbox-blocks .block .w50 { width: 50%; }
+        #richtextbox-blocks .block .w33 { width: 33.333%; }
+        #richtextbox-blocks .block .w25 { width: 25%; }
     </style>
     <div class="wrapper-content">
+        <a class="button" onclick="unsetActiveElement()">deaktivovat</a>
         <div class="tableOptions">
-            <a class="button" onclick="addElement('p')">Text</a>
-            <a class="button" onclick="addElement('h1')">H1</a>
-            <a class="button" onclick="addElement('h2')">H2</a>
-            <a class="button" onclick="addElement('h3')">H3</a>
-            <a class="button" onclick="addElement('h4')">H4</a>
-            <a class="button" onclick="addElement('h5')">H5</a>
-            <a class="button" onclick="displayPopupForm()">img</a>
-            <a class="button" onclick="displayPopupForm2()">html</a>
-            <a class="button" onclick="addSection('w100')">w100</a>
+            <a class="button" onclick="addElement('p', true)">p</a>
+            <a class="button" onclick="addElement('h1', true)">h1</a>
+            <a class="button" onclick="addElement('h2', true)">h2</a>
+            <a class="button" onclick="addElement('h3', true)">h3</a>
+            <a class="button" onclick="addElement('h4', true)">h4</a>
+        </div>
+        <div class="tableOptions">
+            <a class="button" onclick="addElement('section')">section</a>
+            <a class="button" onclick="addElement('div')">div</a>
+            <a class="button" onclick="addElement('div', false, 'w100')">w100</a>
+            <a class="button" onclick="addElement('div', false, 'w50')">w50</a>
+            <a class="button" onclick="addElement('div', false, 'w33')">w33</a>
+            <a class="button" onclick="addElement('div', false, 'w25')">w25</a>
         </div>
 
-        <div class="code-editor">
-            <div class="content" id="code-editor-content">
-            </div>
-            <div class="data">
-                <form id="code-editor-data"></form>
+        <div id="richtextbox">
+            <div class="blocks" id="richtextbox-blocks">
             </div>
         </div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/sortablejs@latest/Sortable.min.js"></script>
     <script>
-        const cec = document.getElementById("code-editor-content")
-        const cee = document.getElementById("code-editor-data")
-        let activeBlock = null
+        const blocks = document.getElementById("richtextbox-blocks")
+        let activeElement = null
+        let activeSubElement = null
 
-        function updateNumbers() {
-            const numArr = document.querySelectorAll(".code-editor .content .block .number")
-            for(i = 0; i < numArr.length; i++) {
-                numArr[i].innerText = i+1
-            }
-        }
-
-        function removeElement(el) {
-            const block = el.closest('.block');
-            if (block) {
-                block.remove();
-            }
-            updateNumbers()
-        }
-
-        function addSection(classname) {
+        function appendToBlocks(element) {
             const div = document.createElement("div")
             div.classList.add("block")
-            div.setAttribute("onclick", "setActiveBlock(this)")
-            div.appendChild(elementNumber())
-            const divWrapper = document.createElement("div")
-            divWrapper.classList.add("wrapper")
-            divWrapper.setAttribute("ondblclick", "changeBlockHtml(this)")
-            divWrapper.setAttribute("isEditing", "false")
-
-            const section = document.createElement("section")
-            section.classList.add(classname)
-            divWrapper.appendChild(section)
-            div.appendChild(divWrapper)
-            div.appendChild(deleteButton())
-
-            cec.appendChild(div)
+            addClickHandle(element)
+            div.appendChild(element)
+            div.setAttribute("isediting", "false")
+            addDoubleClickHandle(div)
+            blocks.appendChild(div)
         }
-
-        function changeBlockHtml(el) {
-            if(el.getAttribute("isediting") === "false") {
-                const innerHtml = el.innerHTML
-                el.innerHTML = ""
+        function addClickHandle(element) {
+            element.setAttribute("onclick", "setActiveElement(this)")
+        }
+        function addClickHandleSubElement(element) {
+            element.setAttribute("onclick", "setActiveSubElement(this)")
+        }
+        function unsetActiveSubElement() {
+            activeSubElement = null
+        }
+        function setActiveSubElement(element) {
+            activeSubElement = element
+            console.log("activce sub element: " + element)
+        }
+        function setActiveElement(element) {
+            activeElement = element
+            console.log("active element: " + element)
+        }
+        function unsetActiveElement() {
+            activeElement = null
+        }
+        function addDoubleClickHandle(element) {
+            element.setAttribute("ondblclick", "displayHTML(this)")
+        }
+        function displayHTML(element) {
+            if(element.getAttribute("isediting") === "false") {
                 const textarea = document.createElement("textarea")
-                textarea.value = innerHtml
-                textarea.setAttribute("style", "resize:auto;")
-                textarea.setAttribute("spellcheck", "false")
-                el.appendChild(textarea)
-                el.setAttribute("isediting", true)
+                textarea.value = element.innerHTML
+                element.innerHTML = ""
+                element.appendChild(textarea)
+                element.setAttribute("isediting", "true")
             } else {
-                el.innerHTML = el.children[0].value
-                el.setAttribute("isediting", false)
+                element.innerHTML = element.children[0].value
+                element.setAttribute("isediting", "false")
             }
         }
-
-
-        function setActiveBlock(block) {
-            activeBlock = block
-            let arr = Array.from(cec.childNodes)
-            arr.shift()
-            arr.forEach(child => {
-                if(child === block) {
-                    child.setAttribute("style", "border-bottom:1px solid rgba(255, 0, 0, 0.2);")
-                } else child.setAttribute("style", "")
-            })
-        }
-
-        function addElement(el) {
-            const div = document.createElement("div")
-            div.classList.add("block")
-            div.setAttribute("onclick", "setActiveBlock(this)")
-            div.appendChild(elementNumber())
-            const divWrapper = document.createElement("div")
-            divWrapper.classList.add("wrapper")
-            divWrapper.setAttribute("ondblclick", "changeBlockHtml(this)")
-            divWrapper.setAttribute("isEditing", "false")
-            if(el === "custom") {
-                divWrapper.innerHTML = document.getElementById("customHtml").value
-            } else if (el === "img") {
-                const img = document.createElement("img")
-                img.src = document.getElementById("imgSrc").value
-                divWrapper.appendChild(img)
-            } else {
-                const element = document.createElement(el)
-                element.setAttribute("contenteditable", "true")
-                element.setAttribute("spellcheck", "false")
-                divWrapper.appendChild(element)
+        function addElement(el, editable, classname) {
+            const element = document.createElement(el)
+            if(editable) {
+                element.setAttribute("contentEditable", "true")
+                element.setAttribute("style","width:100%")
             }
-            div.appendChild(divWrapper)
-            div.appendChild(deleteButton())
-
-            cec.appendChild(div)
-            hidePopupForm()
-            hidePopupForm2()
-        }
-
-        new Sortable(cec, {
-            animation: 150,
-            onEnd: function () {
-                updateNumbers(); // Update numbers after sorting
+            if(classname) {
+                element.classList.add(classname)
             }
-        });
+            if(activeElement) {
+                addClickHandleSubElement(element)
+                activeElement.appendChild(element)
+            } else appendToBlocks(element)
+        }
 
-        function deleteButton() {
-            const button = document.createElement("a")
-            button.innerText = "smazat"
-            button.classList.add("button")
-            button.setAttribute("onclick", "removeElement(this)")
-            return button
-        }
-        function elementNumber() {
-            const span = document.createElement("span")
-            span.innerText = cec.childElementCount+1
-            span.classList.add("number")
-            return span
-        }
     </script>
 </main>
 </body>
